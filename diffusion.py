fixed underflow issues during mixed precision

diffusion.py
@@ -551,10 +551,10 @@ class DiffusionCSDI(ls.Layer, MaskingMixinCSDI):
 
         alpha_hat = 1 - beta
         alpha = np.cumprod(alpha_hat)
-        alpha_tf = tf.cast( tf.constant(alpha), self.compute_dtype)
+        alpha_tf = tf.constant(alpha)[:,tf.newaxis, tf.newaxis]
+
         # alpha_tf = tf.cast( tf.constant(alpha), tf.float64)
-        
-        return tf.constant(beta, dtype=self.compute_dtype), tf.constant(alpha_hat, self.compute_dtype), alpha_tf[:,tf.newaxis, tf.newaxis]
+        return tf.constant(beta, dtype=tf.float32), tf.constant(alpha_hat, tf.float32), tf.cast(alpha_tf, tf.float32)
           
     def call(self, model, observed_data, cond_mask, eval_all_timesteps=False):
 
@@ -599,9 +599,9 @@ class DiffusionCSDI(ls.Layer, MaskingMixinCSDI):
             #Training Loop
             diffusion_steps = tf.experimental.numpy.random.randint(0, self.num_steps, size=(B,))  # randomly sample diffusion steps from 1~T
 
-            current_alpha = tf.gather( self.alpha_tf, diffusion_steps ) # (B,1,1)
-
+            current_alpha = tf.cast( tf.gather( self.alpha_tf, diffusion_steps ), self.compute_dtype) # (B,1,1)
             noise = std_normal( observed_data.shape, self.compute_dtype )
+            
             noisy_data = (current_alpha ** 0.5) * observed_data + (1.0 - current_alpha) ** 0.5 * noise
             cond_obs = (cond_mask * observed_data)[:, tf.newaxis]
 
@@ -621,11 +621,12 @@ class DiffusionCSDI(ls.Layer, MaskingMixinCSDI):
                            
 
         # extending in batch dimension for sampling in one pass through
-        current_sample = std_normal( (B, K, L*pred_samples), dtype)
+        current_sample = std_normal( (pred_samples*B, K, L), dtype)
         observed = tf.cast(tf.repeat(observed, pred_samples, axis=0), dtype)
         cond_mask = tf.cast(tf.repeat(cond_mask, pred_samples, axis=0), dtype)
 
         for t in range(self.num_steps - 1, -1, -1):
+        # for t in range(self.num_steps - 1, 0, -1):
             cond_obs = (cond_mask * observed)[:, tf.newaxis]
             
             # if self.tgt_mask_method == 'tgt_all_remaining':
@@ -644,7 +645,7 @@ class DiffusionCSDI(ls.Layer, MaskingMixinCSDI):
             
             if t > 0:
                 # noise = torch.randn_like(current_sample)
-                noise = std_normal(size, predicted.dtype)
+                noise = std_normal((B*pred_samples,K,L) , predicted.dtype)
                 
                 alpha_tm1 = tf.cast(self.alpha_tf[t-1], dtype)
                 betat = tf.cast(self.beta[t], dtype)
@@ -654,7 +655,6 @@ class DiffusionCSDI(ls.Layer, MaskingMixinCSDI):
                 current_sample += noise * sigma
         
         current_sample = tf.stop_gradient(current_sample) #(b*samples, d, seq)
-        
         
         current_sample = einops.rearrange(current_sample, '(b s) ... -> b ... s', s=pred_samples )
 

train.py
@@ -8,7 +8,7 @@ import argparse
 from argparse import ArgumentParser
 import tensorflow as tf
 
-tf.get_logger().setLevel('ERROR')
+# tf.get_logger().setLevel('ERROR')
 # tf.config.run_functions_eagerly(True)
 physical_devices = tf.config.list_physical_devices('GPU')
 try:
@@ -292,10 +292,9 @@ class ImputationTrainer(tf.keras.Model):
 
             cond_mask=cond_mask,
             pred_samples=self.pred_samples
-        ) # (bs, pred_samples, d, seq )
+        ) # (bs, d, seq, pred_sample )
         
         # Creating expanded versions to match m samples per prediction
-        generated_audio = tf.transpose( generated_audio, (0,2,3,1) ) #(bs, d, seq, pred_sample)
         audio_expanded = tf.broadcast_to( audio[..., tf.newaxis], generated_audio.shape ) 
         loss_mask_expanded = tf.broadcast_to(loss_mask[..., tf.newaxis], generated_audio.shape )
         
@@ -346,7 +345,7 @@ class ImputationTrainer(tf.keras.Model):
         super(ImputationTrainer, self).compile(**kwargs)
         
         self.optimizer = optimizer
-        self.optimizer.jit_compile = kwargs.get( 'jit_compile', False )
+        self.optimizer.jit_compile = False
         self.mixed_precision = mixed_precision
         
         if self.mixed_precision is True:
@@ -402,12 +401,12 @@ class ImputationTrainer(tf.keras.Model):
         callbacks.append(
             ks.callbacks.EarlyStopping(
                 monitor='val_loss',
-                patience=8,
+                patience=10,
                 verbose=1,
                 )
             )
         
-        prog_bar = tfa.callbacks.TQDMProgressBar(metrics_format='{name}: {value:e}')
+        prog_bar = tfa.callbacks.TQDMProgressBar(metrics_format='{name}: {value:.2e}')
         callbacks.append(prog_bar)
 
         callbacks.append(
@@ -443,7 +442,6 @@ class ImputationTrainer(tf.keras.Model):
 
         
         trainer.compile(
-            jit_compile = config_trainer.jit_compile,
             run_eagerly=config_trainer.eager,
             optimizer = optimizer,
             mixed_precision = config_trainer.mixed_precision
@@ -517,8 +515,7 @@ class ImputationTrainer(tf.keras.Model):
 
         callbacks = [
             # ImputationTrainerCallback
-            tfa.callbacks.TQDMProgressBar(metrics_format='{name}: {value:e}')
-,
+            tfa.callbacks.TQDMProgressBar(metrics_format='{name}: {value:.2e}'),
             ImputationTrainerCallback( config_trainer.dir_ckpt, config_trainer.exp_name)
             ]
 
@@ -570,7 +567,6 @@ class ImputationTrainer(tf.keras.Model):
         parser.add_argument("--optimizer", default='adam', type=str, choices=['adam','adafactor'])
         parser.add_argument("--mixed_precision", action='store_true')
         parser.add_argument("--eager", action='store_true') 
-        parser.add_argument("--jit_compile", action='store_true') 
         parser.add_argument("--grad_accum", default=None, type=int)
         
         parser.add_argument("--workers", default=10, type=int )